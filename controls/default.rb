e6b8097a8104adfc34ea0b47082afd4eba1c6ce4
akshayparvatikar174 <apravati@progress.com>
2025-01-29 21:55:22 +0530

    Update default.rb

--- controls/default.rb
+++ controls/default.rb
@@ -2,7 +2,9 @@ control 'nginx-version' do
   impact 1.0
   title 'version check'
   desc 'Check for nginx version'
+  describe.one do
   describe nginx do
     its('version') { should eq '1.25.5' }
   end
+end
 end
